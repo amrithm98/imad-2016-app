deedd585a9662f5a79c83496d40fb9a6aa64ece6
amrith98 <amrithchallenger@gmail.com>
2016-09-28 00:19:19 +0530

    Input ,make requests to server,get response,render it

--- server.js
+++ server.js
@@ -4,29 +4,33 @@ var path = require('path');
 
 var app = express();
 app.use(morgan('combined'));
-var articles={
-    'article-one':{
-        title:'Article 1 | Amrith M',
-    date:'Sept 27',
-    heading:'Page One',
-    content:`<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`},
-    'article-two':{
-        title:'Article 2 | Amrith M',
-    date:'Sept 28',
-    heading:'Page Two',
-    content:`<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`},
-    'article-three':{
-        title:'Article 3 | Amrith M',
-        date:'Sept 29',
-        heading:'Page Three',
-        content:`<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`}
+var articles = {
+    'article-one': {
+        title: 'Article 1 | Amrith M',
+        date: 'Sept 27',
+        heading: 'Page One',
+        content: `<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`
+    },
+    'article-two': {
+        title: 'Article 2 | Amrith M',
+        date: 'Sept 28',
+        heading: 'Page Two',
+        content: `<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`
+    },
+    'article-three': {
+        title: 'Article 3 | Amrith M',
+        date: 'Sept 29',
+        heading: 'Page Three',
+        content: `<p>Renovild is an application to let us know any particular location. This app is a part of out project to rewamp abandoned buildings. In renovild , any user can create and account, search for a location and save that particular location into a database. This database is managed by us </p>`
+    }
 };
-function createTemplate(data){
-    var title=data.title;
-    var heading=data.heading;
-    var date=data.date;
-    var content=data.content;
-var htmlTemplate=`<!DOCTYPE html>
+
+function createTemplate(data) {
+    var title = data.title;
+    var heading = data.heading;
+    var date = data.date;
+    var content = data.content;
+    var htmlTemplate = `<!DOCTYPE html>
 <html>
 <head>
     <title>${title}</title>
@@ -48,32 +52,38 @@ var htmlTemplate=`<!DOCTYPE html>
 `;
     return htmlTemplate;
 }
-app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'index.html'));
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
-var counter=0;
-app.get('/counter', function (req, res) {
-    counter=counter+1;
-     res.send(counter.toString());
+var counter = 0;
+app.get('/counter', function(req, res) {
+    counter = counter + 1;
+    res.send(counter.toString());
 });
-app.get('/:articleName',function(req,res){
-    var articleName=req.params.articleName;
+var names=[];
+app.get('/submit-name', function(req, res) {
+    var name=req.query.name;// /submit-name?name="amrith"
+    names.push(name);
+    res.send(JSON.stringify(names));
+});
+app.get('/:articleName', function(req, res) {
+    var articleName = req.params.articleName;
     res.send(createTemplate(articles[articleName]));
-    
+
 });
 
-app.get('/ui/style.css', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'style.css'));
+app.get('/ui/style.css', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/madi.png', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
+app.get('/ui/madi.png', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
-app.get('/ui/main.js', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'main.js'));
+app.get('/ui/main.js', function(req, res) {
+    res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
-    
+
 var port = 8080; // Use 8080 for local development because you might already have apache running on 80
-app.listen(8080, function () {
-  console.log(`IMAD course app listening on port ${port}!`);
+app.listen(8080, function() {
+    console.log(`IMAD course app listening on port ${port}!`);
 });
--- ui/main.js
+++ ui/main.js
@@ -11,6 +11,33 @@ button.onclick = function() {
         }
     };
     //make req
-   	request.open('GET',"http://localhost:8080/counter",true);
-   	request.send(null);
+    request.open('GET', "http://localhost:8080/counter", true);
+    request.send(null);
 }
+
+var submit = document.getElementById('Submit');
+submit.onclick = function() {
+    //make a req and send name
+    //get list of names as response and put it to the unordered list
+    var request = new XMLHttpRequest(); //req made
+    request.onreadystatechange = function() {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if (request.status === 200) {
+                var names = request.responseText;
+                names = JSON.parse(names);
+                var list = '';
+                for (var i = 0; i < names.length; i++) {
+                    list += '<li>' + names[i] + '</li>';
+                }
+                var ul = document.getElementById('namelist');
+                ul.innerHTML = list;
+
+            }
+        }
+    };
+    //make req
+    var nameInput = document.getElementById('namein');
+    var name = nameInput.value;
+    request.open('GET', "http://localhost:8080/submit-name?name=" + name, true);
+    request.send(null);
+};
